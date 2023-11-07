d4afc1a8ea073ce97713f251ae06f2ea570c59d2
MoOsman5 <mohamed1592000gmail.com>
2023-11-08 01:02:55 +0200

    create house

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
 const express = require("express");
 require("dotenv").config();
-const {PrismaClient} =require("@prisma/client")
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
-const prisma =new PrismaClient()
-const PORT = process.env.PORT ;
+const prisma = new PrismaClient();
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  const allUsers = await prisma.user.findMany()
-  res.json(allUsers)
+  const allUsers = await prisma.user.findMany();
+  res.json(allUsers);
 });
 app.post("/", async (req, res) => {
-  const newUser = await prisma.user.create({data:req.body})
-  res.json(newUser)
-
+  const newUser = await prisma.user.create({ data: req.body });
+  res.json(newUser);
 });
 app.put("/:id", async (req, res) => {
-  const id=Number(req.params.id);
-  const newAge=req.body.age
+  const id = Number(req.params.id);
+  const newAge = req.body.age;
   const updatedUser = await prisma.user.update({
-    where:{id:id},
-    data:{age:newAge}
-  })
-  res.json(updatedUser)
-
+    where: { id: id },
+    data: { age: newAge },
+  });
+  res.json(updatedUser);
 });
 app.delete("/:id", async (req, res) => {
-  const id=Number(req.params.id);
+  const id = Number(req.params.id);
   const deletedUser = await prisma.user.delete({
-    where:{id:id},
+    where: { id: id },
+  });
+  res.json(deletedUser);
+});
 
-  })
-  res.json(deletedUser)
 
+
+app.post("/house", async (req, res) => {
+  const newHouse = await prisma.house.create({data:req.body})
+  res.json(newHouse)
 });
 
 app.listen(PORT, () => {
